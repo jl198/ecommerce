9bfbc8819714f7d5851ae28cba4aebf2d6d31c6f
Jacob Leiner <jacob.leiner@gmail.com>
2022-02-22 08:34:20 -0800

    created routes in django and linked react to api endpoints

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,29 @@
-import React from "react"
-import { Row, Col } from 'react-bootstrap'
+import React, {useState, useEffect} from "react"
+import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
-import products from '../products'
 
 function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        async function fetchProducts() {
+            const { data } = await axios.get('/api/products/')
+            setProducts(data)
+        }
+
+        fetchProducts()
+
+    }, [])
     return (
         <div>
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} Lg={4} xL={3}>
-                        <Product product={product} />
+                        <Product product={product}/>
                     </Col>
                 ))}
             </Row>
