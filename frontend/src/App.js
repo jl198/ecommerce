2ec2a497cf84a02c30ec3a2284a736d82a873ada
Jacob Leiner <jacob.leiner@gmail.com>
2022-02-22 02:52:50 -0800

    finished productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import { Container } from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <Header/>
             <main className={"py-3"}>
                 <Container>
-                    <HomeScreen />
+                    <Routes>
+                        <Route path='/' element={<HomeScreen />} />
+                        <Route path='/product/:id' element={<ProductScreen />} />
+                    </Routes>
                 </Container>
             </main>
             <Footer/>
-        </div>
-    );
+        </BrowserRouter>
+    )
 }
 
 export default App;
